1fae0a854187118058889227cf62cc24ee196e33
johnkjrfc <johnkjrfc@gmail.com>
2024-01-17 10:25:14 -0600

    project complete

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -13,12 +13,13 @@ import java.util.List;
 /**
  * Created by LaunchCode
  */
+//Public makes this accessible in the rest of program. imports data from csv, stores utility methods for searches
 public class JobData {
 
-    private static final String DATA_FILE = "src/main/resources/job_data.csv";
+    private static final String DATA_FILE = "src/main/resources/job_data.csv";  //private static final, so it CANNOT be changed anywhere else
     private static boolean isDataLoaded = false;
 
-    private static ArrayList<HashMap<String, String>> allJobs;
+    private static ArrayList<HashMap<String, String>> allJobs;  //declare ArrayList of HashMap objects, allJobs
 
     /**
      * Fetch list of all values from loaded data,
@@ -27,13 +28,14 @@ public class JobData {
      * @param field The column to retrieve values from
      * @return List of all of the values of the given field
      */
+    //utility method that lists by field
     public static ArrayList<String> findAll(String field) {
 
         // load data, if not already loaded
         loadData();
 
         ArrayList<String> values = new ArrayList<>();
-
+        //for row in alljobs,
         for (HashMap<String, String> row : allJobs) {
             String aValue = row.get(field);
 
@@ -41,7 +43,7 @@ public class JobData {
                 values.add(aValue);
             }
         }
-
+        Collections.sort(values);
         return values;
     }
 
@@ -49,7 +51,7 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
-
+//        Collections.sort(allJobs);
         return allJobs;
     }
 
@@ -64,17 +66,18 @@ public class JobData {
      * @param value Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
+    //User selects search by a column
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
 
         // load data, if not already loaded
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
-
+        //for row in all jobs
         for (HashMap<String, String> row : allJobs) {
-
+            //retrieve aValue from each row in the selected column
             String aValue = row.get(column);
-
+            //if aValue matches value (case insensitive), add that row to jobs array
             if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
@@ -89,25 +92,24 @@ public class JobData {
      * @param value The search term to look for
      * @return      List of all jobs with at least one field containing the value
      */
+    //User selects 'search' and 'all'
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
-        //System.out.println("findByValue method called");
-
         // load data, if not already loaded
         loadData();
-        //System.out.println("Loaded data, allJobs size: " + allJobs.size());
+        //declare empty array for our search matches
         ArrayList<HashMap<String, String>> searchMatches = new ArrayList<>();
-//        value = value.trim().toLowerCase();
-        //System.out.println("Searching for: " + value);
 
+
+        //for row in allJobs
         for (HashMap<String, String> row : allJobs) {
+            //iterate through keys in allJobs
             for (String key : row.keySet()) {
                 String columnValue = row.get(key);
 
                 if (columnValue != null) {
-//                    columnValue = columnValue.trim().toLowerCase();
-                    //
-                    //System.out.println("comparing: " + columnValue.toLowerCase() + " with: " + value.toLowerCase());
+                    //check if columnValue contains value
                     if (columnValue.toLowerCase().contains(value.toLowerCase())) {
+                        //check that searchMatches doesn't already have this row, if not row is added
                         if (!searchMatches.contains(row)) {
                             searchMatches.add(row);
                             break;
@@ -131,23 +133,29 @@ public class JobData {
         if (isDataLoaded) {
             return;
         }
-
+        //!isDataLoaded, attempts to format csv. try avoids IO exceptions
         try {
 
             // Open the CSV file and set up pull out column header info and records
-            Reader in = new FileReader(DATA_FILE);
-            CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-            List<CSVRecord> records = parser.getRecords();
-            Integer numberOfColumns = records.get(0).size();
-            String[] headers = parser.getHeaderMap().keySet().toArray(new String[numberOfColumns]);
+            Reader in = new FileReader(DATA_FILE); //read
+            CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in); //first record in 'in' used as header
+            List<CSVRecord> records = parser.getRecords(); //retrieve all records
+            Integer numberOfColumns = records.get(0).size(); //determine no. columns in 'in'
 
+            //string array. retrieve map<String, Int>. grab the keys. create array from a new String length of numcolumns
+            String[] headers = parser.getHeaderMap().keySet().toArray(new String[numberOfColumns]); //columns to array
+
+            //init all jobs as empty arrayList which will store a hashMap of each job in csv
             allJobs = new ArrayList<>();
 
             // Put the records into a more friendly format
+            //for record in records
             for (CSVRecord record : records) {
+                //init a new empty hashMap
                 HashMap<String, String> newJob = new HashMap<>();
-
+                //for header in headers [],
                 for (String headerLabel : headers) {
+                    //put the header as key, and record @ header column as value in newJob
                     newJob.put(headerLabel, record.get(headerLabel));
                 }
 
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -12,7 +12,7 @@ public class TechJobs {
 
     public static void main (String[] args) {
 
-        // Initialize our field map with key/name pairs
+        // Initialize our field map with key/name pairs. Essentially just the headers of our CSV, template for later outputs
         HashMap<String, String> columnChoices = new HashMap<>();
         columnChoices.put("core competency", "Skill");
         columnChoices.put("employer", "Employer");
@@ -20,7 +20,7 @@ public class TechJobs {
         columnChoices.put("position type", "Position Type");
         columnChoices.put("all", "All");
 
-        // Top-level menu options
+        // initialize Top-level menu options
         HashMap<String, String> actionChoices = new HashMap<>();
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
@@ -29,24 +29,26 @@ public class TechJobs {
 
         // Allow the user to search until they manually quit
         while (true) {
-
             String actionChoice = getUserSelection("View jobs by (type 'x' to quit):", actionChoices);
 
+            //break if input is x in getUserSelection instance
             if (actionChoice == null) {
                 break;
+            //user input is list, generate next UI selection for list using columns in csv
             } else if (actionChoice.equals("list")) {
-
                 String columnChoice = getUserSelection("List", columnChoices);
 
+                // loops for next user input (Explain both findAll methods
+                //user inputs 0 for all
                 if (columnChoice.equals("all")) {
                     printJobs(JobData.findAll());
+                //user input another valid input
                 } else {
-
                     ArrayList<String> results = JobData.findAll(columnChoice);
 
                     System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
 
-                    // Print list of skills, employers, etc
+                    // Print list value in selected column
                     for (String item : results) {
                         System.out.println(item);
                     }
@@ -54,7 +56,7 @@ public class TechJobs {
 
             } else { // choice is "search"
 
-                // How does the user want to search (e.g. by skill or employer)
+                //print UI instructions for search
                 String searchField = getUserSelection("Search by:", columnChoices);
 
                 // What is their search term?
@@ -71,11 +73,12 @@ public class TechJobs {
     }
 
     // ﻿Returns the key of the selected item from the choices Dictionary
+    //Utility Method that sets up a menu header for a hashmap of choices for the UI
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
-        int choiceIdx = -1;
-        Boolean validChoice = false;
-        String[] choiceKeys = new String[choices.size()];
+        int choiceIdx = -1;     //User choice variable. init at -1, which is invalid, before user input received.
+        Boolean validChoice = false;    //variable used as flag to see if input is valid. start false
+        String[] choiceKeys = new String[choices.size()];   //stores keys of choices, utilizing .size to accommodate changes in choices.
 
         // Put the choices in an ordered structure so we can
         // associate an integer with each one
@@ -84,19 +87,20 @@ public class TechJobs {
             choiceKeys[i] = choiceKey;
             i++;
         }
-
+        //Do-while valid choice is false(default) to make this block execute at least once.
         do {
 
             System.out.println("\n" + menuHeader);
 
-            // Print available choices
+            // Print available choices index and value
             for (int j = 0; j < choiceKeys.length; j++) {
                 System.out.println("" + j + " - " + choices.get(choiceKeys[j]));
             }
-
+            //scanner if User input is an integer, update choice idx to input
             if (in.hasNextInt()) {
                 choiceIdx = in.nextInt();
                 in.nextLine();
+            //User enters x, signal to quit is sent
             } else {
                 String line = in.nextLine();
                 boolean shouldQuit = line.equals("x");
@@ -113,7 +117,7 @@ public class TechJobs {
             }
 
         } while(!validChoice);
-
+        // returns the index the user selected from choiceKeys
         return choiceKeys[choiceIdx];
     }
 
